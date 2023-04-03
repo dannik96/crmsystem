0edb5daefa631c07a1cd8dc8b9bd565cb01b00f7
Daniel Groschup <daniel.groschup@email.cz>
2023-04-03 07:56:10 +0200

    Create controllers basics

--- src/main/java/cz/cvut/fel/groscdan/crmsystem/repository/project/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package cz.cvut.fel.groscdan.crmsystem.repository.project;
-
-import cz.cvut.fel.groscdan.crmsystem.model.crm.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
